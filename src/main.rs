27dbdf31bbfd687e7a8915abb6f03fa0b9ddbcf2
wyfcyx <wyfcyx@gmail.com>
2019-09-21 23:39:37 +0800

    post-03 completed

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,20 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello world!";
-
+mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
-        }
-    }
-
-    loop {}
+    /*
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    */
+    println!("Hello world! 1 + 1 = {}", 1 + 1);
+    panic!("some panic message...");
 }
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
